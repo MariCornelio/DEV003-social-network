08c781cf9500b540f0207e81e6e8fc0b2cdf6bb6
LittlePuggers <anaim.ecologia@gmail.com>
2023-01-26 15:41:42 -0700

    creamos el ruteado y la vista login

--- src/lib/login.js
+++ src/lib/login.js
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>devgram</title>
-
-  <!--Login-->
-</head>
-
-<body>
-
-
-  <!-- ===== Icon ===== -->
-  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-
-  <!-- ===== CSS ===== -->
-  <link rel="stylesheet" href="style.css">
-
-  <!--<title>Login & Registration Form</title>-->
-  </head>
-
-  <body>
+export const login = `
 
     <div class="container">
       <div class="forms">
@@ -57,7 +32,7 @@
 
           <div class="login-signup">
             <span class="text">Haven't created an account?
-              <a href="#" class="text signup-link">Signup Now</a>
+              <a class="text signup-link">Signup Now</a>
             </span>
           </div>
         </div>
@@ -99,14 +74,13 @@
 
           <div class="login-signup">
             <span class="text">Already have an account?
-              <a href="#" class="text login-link">Login Now</a>
+              <a class="text login-link">Login Now</a>
             </span>
           </div>
         </div>
       </div>
     </div>
 
-    <!--<script src="script.js"></script>-->
-  </body>
+    <script src="script.js"></script>
 
-</html>
+`;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
+import { router } from './lib/router.js';
 
-myFunction();
+// myFunction();
+
+router();
+window.addEventListener('hashchange', () => {
+  router();
+});
