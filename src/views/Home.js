c8f766579446768f70887fceaef59085d7be1f0c
LittlePuggers <anaim.ecologia@gmail.com>
2023-02-07 09:33:43 -0700

    Fixed home header

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,12 +6,14 @@ export const Home = () => {
   <header class="home-header">
     <div class="home-logo">devgram<></div>
     <div class="home-buttons">
-      <div class="home-create-btn">
-        <span>&#10022;</span>
-        <p>Create</p>
+      <div class="home-post-btn">
+        <i class="fa-solid fa-plus"></i>
+        <p>Post</p>
       </div>
       <div class="home-edit-profile">
-        <span>&#9786;</span> 
+        <div class="home-profile-pic">
+          <img src="../assets/profile.jpg" alt="Profile photo"> 
+        </div>
         <p>Edit</p>
       </div>
     </div>
@@ -19,7 +21,7 @@ export const Home = () => {
 
     <!-- menu -->
     <!-- Fer-Main -->
-    <main>
+    <main class="main-content">
     <section class="home-stories">
 
       <!-- HTML solo para demostracion -->
