fcca86743a21883e74d5aca111a5598d7111fe2b
LittlePuggers <anaim.ecologia@gmail.com>
2023-02-09 17:39:27 -0700

    added register test

---
+++ test/register.spec.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { Login } from '../src/views/Login.js';
+import { loginLogic } from '../src/controllers/loginLogic.js';
+
+describe('register form', () => {
+  it('should render form correctly', () => {
+    // GIVEN
+    const rootDiv = document.createElement('div');
+    rootDiv.id = 'root';
+
+    document.body.append(rootDiv);
+
+    // WHEN
+    rootDiv.appendChild(Login());
+    loginLogic();
+
+    // THEN
+    const formElement = document.querySelector('#form-signin');
+    // inputs
+    // boton
+
+    expect(formElement).not.toBeNull();
+    console.log(document.body.innerHTML);
+  });
+});
