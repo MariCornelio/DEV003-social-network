2c486ac20dff7acdf7cd443d4b3b92785a7b9e89
LittlePuggers <anaim.ecologia@gmail.com>
2023-02-13 07:43:31 -0700

    added dropdown menu to post

--- src/controllers/homeLogic.js
+++ src/controllers/homeLogic.js
@@ -1,47 +1,65 @@
 export const homeLogic = () => {
-
-const addInterationsToPost = (post) => {
+  const addInterationsToPost = (post) => {
     // post like
 
     let likeBtn = post.querySelector('.like-btn');
     let likeImg = post.querySelector('.like-icon');
 
     likeBtn.addEventListener('click', () => {
-        if(likeBtn.src.includes('nofill')){
-            likeImg.classList.add('show');
-            if(shareBtn.src.includes('-fill')){
-                shareBtn.click();
-            }
+      if (likeBtn.src.includes('nofill')) {
+        likeImg.classList.add('show');
+        if (shareBtn.src.includes('-fill')) {
+          shareBtn.click();
         }
+      }
 
-        changeIcon(likeBtn);
+      changeIcon(likeBtn);
 
-        setTimeout(() => {
-            likeImg.classList.remove('show');
-        }, 3000);
-    })
+      setTimeout(() => {
+        likeImg.classList.remove('show');
+      }, 3000);
+    });
 
     // post share
     let shareBtn = post.querySelector('.send-btn');
     let shareWindow = post.querySelector('.share-window');
 
     shareBtn.addEventListener('click', () => {
-        shareWindow.classList.toggle('active');
-        changeIcon(shareBtn)
-    })
+      shareWindow.classList.toggle('active');
+      changeIcon(shareBtn);
+    });
 
     let postLink = post.querySelector('#share-link').value;
     let copyLinkBtn = post.querySelector('.copy-btn');
 
     copyLinkBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(postLink).then(() => {
-            shareBtn.click();
-        })
-    })
-}
+      navigator.clipboard.writeText(postLink).then(() => {
+        shareBtn.click();
+      });
+    });
+  };
 
-// post
+  // post
 
-let posts = [...document.querySelectorAll('.post')];
-posts.map(post => addInterationsToPost(post));
+  let posts = [...document.querySelectorAll('.post')];
+  posts.map((post) => addInterationsToPost(post));
+
+  // show post dropdown menu
+  const postMenu = document.querySelector('.post-actions');
+  postMenu.addEventListener('click', () => {
+    document.getElementById('myDropdown').classList.toggle('show-post-actions');
+  });
+
+  // close post dropdown menu
+  window.onclick = function (event) {
+    if (!event.target.matches('.post-actions')) {
+      const dropdowns = document.getElementsByClassName('dropdown-content');
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show-post-actions')) {
+          openDropdown.classList.remove('show-post-actions');
+        }
+      }
+    }
+  };
 };
